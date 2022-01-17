1bef837a770d136084a592026f547f24fb0fe52b
kazi Arif <coderkaziarif@gmail.com>
2022-01-17 17:04:42 +0600

    Contact form does't working to live server

--- mail.php
+++ mail.php
@@ -1,24 +1,44 @@
-<? php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+<?php
 
-$email_from = 'info@vividtechnologybd.com';
-$email_subject = 'New Mail';
-$email_body = 'User Name: $name.\n',
-                "User Email: $visitor_email.\n",
-                "Subject: $subject.\n",
-                "User Message: $message.\n";
+if(isset($_POST['submit'])) {
 
-$to = 'vividtechnologybd@gmail.com';
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-$headers = "From : $email_from \r\n";
+    $emailTo = "info@vividtechnologybd.com"
+    $headers = "From: ".$visitor_email
+    $text = "You have received an e-mail from ".$name.".\n\n".$message;
 
-$headers = "Replay-to: $visitor_email \r\n";
+    // Send email
+    mail($emailTo, $subject, $text, $headers);
 
-mail($to,$email_subject,$email_body,$headers);
+    // redirect
+    header("location:thankyou.html");
+}
 
-// redirect
-header ("location: thankyou.html");
+
+
+// $name = $_POST['name'];
+// $visitor_email = $_POST['email'];
+// $subject = $_POST['subject'];
+// $message = $_POST['message'];
+
+// $email_from = 'info@vividtechnologybd.com';
+// $email_subject = 'New Mail';
+// $email_body = 'User Name: $name.\n',
+//                 "User Email: $visitor_email.\n",
+//                 "Subject: $subject.\n",
+//                 "User Message: $message.\n";
+
+// $to = 'vividtechnologybd@gmail.com';
+
+// $headers = "From : $email_from \r\n";
+
+// $headers = "Replay-to: $visitor_email \r\n";
+
+// mail($to,$email_subject,$email_body,$headers);
+
+
 ?>
